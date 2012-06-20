32d9fd351711577fd21270857a6771d4c2c839d1
Max Meyer <dev@fedux.org>
2012-06-20 07:04:18 +0200

    less whitespace is good whitespace

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,5 @@ require 'tables'
 include Tables
 
 RSpec.configure do |c|
-    c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.treat_symbols_as_metadata_keys_with_true_values = true
 end
