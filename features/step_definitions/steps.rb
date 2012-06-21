530a1726ceb93699e7e27feeeecf9345b104ca57
Max Meyer <dev@fedux.org>
2012-06-21 17:21:10 +0200

    update step definitions for easier testing

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,16 +1,8 @@
 #encoding: utf-8
 When /^I view them as a "(.*?)"$/ do |view|
-  pending # express the regexp above with the code you wish you had
+  binding.pry
 end
 
 Then /^I see the following output on "(.*?)":$/ do |output, string|
   pending # express the regexp above with the code you wish you had
 end
-
-#Given /^the following "([^ ]+)s" exist:$/i do |factory,table|
-#  table.hashes.each do |element|
-#    binding.pry
-#    FactoryGirl.build(factory)
-#    #attributes = FactoryGirl.attributes_for(factory.to_sym)
-#  end
-#end
