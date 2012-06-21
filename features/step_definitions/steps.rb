e76b338f235c2cbee84c52a419da3f55872e9a76
Max Meyer <dev@fedux.org>
2012-06-21 18:27:18 +0200

    capture output for testing

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,8 +1,13 @@
 #encoding: utf-8
 When /^I view them as a "(.*?)"$/ do |view|
-  binding.pry
+  @captured_output = capture_stdout do
+    case view 
+    when "horizontal_table"
+      horizontal_table @factories
+    end
+  end
 end
 
-Then /^I see the following output on "(.*?)":$/ do |output, string|
-  pending # express the regexp above with the code you wish you had
+Then /^I see the following output:$/ do |string|
+  @captured_output.should == string
 end
