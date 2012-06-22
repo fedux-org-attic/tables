a990d008637d0f31d2bd542964a04938a2730161
Max Meyer <dev@fedux.org>
2012-06-22 17:15:30 +0200

    view data in horizontal table

--- lib/tables.rb
+++ lib/tables.rb
@@ -2,5 +2,21 @@ require 'command_line_reporter'
 
 require 'tables/version'
 require 'tables/base'
+require 'tables/helper'
+require 'tables/table_definition'
 require 'tables/horizontal_table'
 require 'tables/detail_table'
+
+# main module
+module Tables
+  
+  # Displays data as a horizontal table
+  # 
+  # @example
+  # 
+  def horizontal_table(data=[],options={})
+    HorizontalTable.run(data,options)
+  end
+
+
+end
