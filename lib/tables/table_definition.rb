24754bfdfbfb078ed1637c598c73fc747afbd4b5
Max Meyer <dev@fedux.org>
2012-06-22 17:16:46 +0200

    better separation of logic

--- lib/tables/table_definition.rb
+++ lib/tables/table_definition.rb
@@ -4,8 +4,19 @@
 module Tables
 
   #hide the nasty stuff
-  #of building a row
-  class RowDefinition
+  #of building a table
+  class TableDefinition
+    def initialize(items = [])
+      @items = items
+    end
+    
+    def attributes
+      first_item = @items.first
+      instance_vars = first_item.instance_variables
+      candidates = instance_vars.map{|variable| variable.to_s.gsub(/@/, '').to_sym}
+
+      method_names = candidates.keep_if {|name| first_item.respond_to?(name) }
+    end
 
   end
 
